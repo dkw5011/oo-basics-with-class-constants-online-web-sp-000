c530f178d10339edf9ef4f88dc6dc6ed31c93ed8
Danielle Bergbower <dkw5011@gmail.com>
2019-06-05 00:10:23 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -13,5 +13,9 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-  BRANDS << brand
+  def brand=(brand)
+    @brand = brand
+    BRANDS << brand
+  end
+  
 end
