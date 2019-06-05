81f85f8d005cd09092be31bf8bd81edeb99a0669
Danielle Bergbower <dkw5011@gmail.com>
2019-06-05 00:01:44 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,7 @@ class Shoe
   
   def initialize(brand)
     @brand = brand
-    
+    BRANDS << brand
   end
 
 
@@ -14,9 +14,4 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
-def brand=(brand)
-  @brand = brand
-  
-end
 end
